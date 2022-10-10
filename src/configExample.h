46d6c14c9db8e610e551e8f3e39a202214ec0f7b
Nikita <s2015836@x.donstu.ru>
2022-10-10 11:24:25 +0300

    Refactor config

--- src/configExample.h
+++ src/configExample.h
@@ -1,10 +1,8 @@
 #pragma once
 
-struct {
-    const char* ssid = "название wi-fi";
-    const char* wifiPassword = "";
-    const char* host = "";
-    const char* apiKey = "";
-    const char* email = "email под которым esp будет авторизовываться";
-    const char* fbPassword = "пароль пользователя";
-} config;
+#define SSID "название wi-fi"
+#define WIFI_PASSWORD ""
+#define HOST "ссылка на базу данных"
+#define API_KEY "ключ для доступа к базе данных"
+#define EMAIL "email под которым esp будет авторизовываться"
+#define FB_PASSWORD "пароль пользователя"
